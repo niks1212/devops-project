2c2f4d231c34f99e97033479dc2e4593a68370aa
NikhilG1112 <Nikhil_Gorade@humanclod.info>
2025-08-30 15:01:52 +0530

    Node.js app working and Dockerized

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const app = express();
 const PORT = 3000;
 
+// Home route
 app.get('/', (req, res) => {
     res.send('Hello DevOps!');
 });
 
+// Start server
 app.listen(PORT, () => {
-    console.log(Server running on port );
+    console.log(`Server running on port ${PORT}`);
 });
